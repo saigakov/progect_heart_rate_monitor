2183e28bbbf90dda223fab0cca71349ca1ac73ba
Konstantin <saigakov@yandex.ru>
2020-05-15 19:52:02 +0300

    add server form NO error

--- src/js/script.js
+++ src/js/script.js
@@ -96,6 +96,8 @@ $(document).ready(function(){
             data: $(this).serialize()
         }).done(function() {
             $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
 
             $('form').trigger('reset');
         });
--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -14,12 +14,12 @@ $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.yandex.ru';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'saigakov@yandex.ru';                 // Наш логин
-$mail->Password = '833Mi8bort739';                           // Наш пароль от ящика
+$mail->Password = '833Mi8Bort739';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('saigakov', 'Pulse');   // От кого письмо 
-$mail->addAddress('saigakov@yandex.ru');     // Add a recipient
+$mail->setFrom('saigakov@yandex.ru', 'Pulse');   // От кого письмо 
+$mail->addAddress('saigakovka@gmail.com');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
