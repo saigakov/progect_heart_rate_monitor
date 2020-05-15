48e79accc3f1bbb021b4e85b1ba657a44d0cd993
Konstantin <saigakov@yandex.ru>
2020-05-15 16:27:35 +0300

    add sending letter

--- src/js/script.js
+++ src/js/script.js
@@ -86,6 +86,22 @@ $(document).ready(function(){
 
     $('input[name=phone]').mask("+7 (999) 999-99-99");
 
+    //sending letter
+    
+    $('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+
   });
 
   window.addEventListener('DOMContentLoaded', function () {
