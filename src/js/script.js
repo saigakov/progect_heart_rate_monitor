296354c299592e8869f9e4e3722eae4c60527a5b
Konstantin <saigakov@yandex.ru>
2020-05-18 00:59:11 +0300

    add slider to jquery

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
-    /* 
-    carusel jquery
+    
+    //carusel jquery
     $('.carousel__inner').slick({
         speed: 1200,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"</button>',
@@ -15,7 +15,7 @@ $(document).ready(function(){
             }
 
         ]
-      }); */
+      });
 
     $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
         $(this)
@@ -123,51 +123,3 @@ $(document).ready(function(){
     new WOW().init();
 
   });
-
-  window.addEventListener('DOMContentLoaded', function () {
-    "use strict";
-
-    //slider
-    function slider() {   
-        let slideIndex = 1,
-            slides = document.querySelectorAll('.slider__item'),
-            prev = document.querySelector('.slider__prev'),
-            next = document.querySelector('.slider__next');
-    
-            showSlides(slideIndex);
-            
-            function showSlides(n) {
-    
-                if (n > slides.length) {
-                    slideIndex = 1;
-                }
-                if (n < 1) {
-                    slideIndex = slides.length;
-                }
-    
-                slides.forEach((item) => item.style.display = 'none');
-    
-                slides[slideIndex - 1].style.display = 'block';
-                }
-    
-            function plusSlides(n) {
-                showSlides(slideIndex += n);
-            }  
-            function currentSlide(n) {
-                showSlides(slideIndex = n);
-            }
-    
-            prev.addEventListener('click', function() {
-                plusSlides(-1);
-            });
-    
-            next.addEventListener('click', function() {
-                plusSlides(1);
-            });
-    
-        };
-
-
-    slider();
-
-});
